2f945acd2bd06db96d6cbc55230ea4589230346e
Anandhavalli <ganandhavalli1@gmail.com>
2024-09-19 19:36:00 +0530

    Update database configuration for Platform.sh

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -159,15 +159,6 @@ if config.is_valid_platform():
                 'PORT': db_settings['port'],
     },
 }
-        DATABASES = {
-                  'default': {
-                       'ENGINE': 'django.db.backends.postgresql',
-                       'NAME': 'your_db_name',
-                       'USER': 'your_db_user',
-                       'PASSWORD': 'your_db_password',
-                       'HOST': 'your_db_host',
-                       'PORT': 'your_db_port',
-    }
-}
+    
 
         
